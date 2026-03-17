542f457cf81f4dcfac93f62c2e1e288cd854310a
Deepak9551 <144364392+Deepak9551@users.noreply.github.com>
2026-03-17 13:57:30 +0530

    old process

--- src/main/java/com/dk/twoPointer/UniquePair.java
+++ src/main/java/com/dk/twoPointer/UniquePair.java
@@ -1,4 +1,40 @@
 package com.dk.twoPointer;
 
+import java.util.Arrays;
+
 public class UniquePair {
+
+    public static void main(String[] args) {
+uniqueArray(new int[]{1,1,2,2,3,4,4});
+    }
+    public static void uniqueArray(int[] nums){
+        int officer = 0;
+        int cm  = 1;
+        int unique = 1;
+        while(cm<nums.length) {
+            if (nums[officer] == nums[cm]) {
+                System.out.println("Skipping " + nums[cm] + " as it is duplicate");
+                cm++;
+                continue;
+            }
+
+            else if (nums[officer] != nums[cm]) {
+                swap(nums, officer + 1, cm);
+                officer++;
+                cm++;
+                unique++;
+            }
+
+
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void swap(int[] arr , int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
 }
